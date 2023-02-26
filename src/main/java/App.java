882419d1aae608174e753c0903706398bf06175e
Amine Haddou <haddouamine2002@gmail.com>
2023-02-26 17:17:23 +0300

    - Creation debut Menu

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,28 +2,39 @@ import javax.swing.*;
 import java.awt.*;
 
 public class App extends JFrame {
-    int min_width ; //largeur minimale de la fenêtre
-    int min_height ; // hauteur minimale de la fenêtre
-    String title ; //titre de la fenêtre
-    Grille grille ; //c’est la grille qui modélise la forêt
-    Menu menu ; //c’est le panneau contenant le menu de l’application
-    int densite; //densite des arbre dans la forêt en pourcentage
-    String direction_vent ; //indique la direction du vent (NORD, SUD, EST, OUEST)
-    String Saison ; //HIVER,PRINTEMPS,ETE,AUTOMNE
-    int taux_arbres_peu_inflammable ; //taux en pourcentage d’arbre à faible inflammabilité
-    int taux_cellules_humides ; //taux en pourcentage de cellules ayant un sol humide
-    int taux_foret_deja_brulee ; //taux en pourcentage de forêt déjà brulée
+    private int min_width ; //largeur minimale de la fenêtre
+    private int min_height ; // hauteur minimale de la fenêtre
+    private String title ; //titre de la fenêtre
+    private JPanel appPanel;
+    private Grille grille ; //c’est la grille qui modélise la forêt
+    private Menu menu ; //c’est le panneau contenant le menu de l’application
+    private int densite; //densite des arbre dans la forêt en pourcentage
+    private String direction_vent ; //indique la direction du vent (NORD, SUD, EST, OUEST)
+    private String Saison ; //HIVER,PRINTEMPS,ETE,AUTOMNE
+    private int taux_arbres_peu_inflammable ; //taux en pourcentage d’arbre à faible inflammabilité
+    private int taux_cellules_humides ; //taux en pourcentage de cellules ayant un sol humide
+    private int taux_foret_deja_brulee ; //taux en pourcentage de forêt déjà brulée
 
-
-    App (){
+    /**
+     * Cette classe est une extension (« extend ») de la classe JFrame de Java
+     * Elle permet de creer la fenetre et puis d'y ajouter les Panel Menu et Grille
+     * Puis de lancer la simulation
+     */
+    public App (int minWidth, int minHeight){
+        min_width = minWidth;
+        min_height = minHeight;
+        menu = new Menu();
+        grille = new Grille();
+        appPanel = new JPanel();
         fenetreApp();
+        panelsApp();
     }
 
     /**
      * methode pour creer la fenetre graphique de l'application
      */
-    void fenetreApp(){
-        this.setMinimumSize(new Dimension(500, 300));
+    private void fenetreApp(){
+        this.setMinimumSize(new Dimension(min_width, min_height));
         this.setLocation(0, 0);
         this.setTitle("Simulation Feu de Foret");
         ImageIcon img = new ImageIcon("D:\\haddo\\Documents\\info\\JavaProjects\\Simulation_Feu_de_Foret\\design\\icone.png"); //Icone fenetre (corner gauche)
@@ -34,7 +45,16 @@ public class App extends JFrame {
         this.setVisible(true);
     }
 
+    /**
+     * Methode pour creer les Panels et les ajouters dans un seul panel. On ajoutera ce panel a la fenetre.
+     */
+    private void panelsApp(){
+
+        this.add(grille);
+        this.add(menu);
+        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+    }
     public static void main(String[] args){
-        new App();
+        new App(500, 500);
     }
 }
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,8 +1,36 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
 
 public class Menu extends JPanel {
+    private int largeur ; // largeur minimale du panneau
+    private int hauteur ; // hauteur minimale du panneau
+    private final static String COULEUR_BOUTON_GENERER = "#47B9F3"; // couleur en hexadecimal du bouton « Générer ».
+    private final static String COULEUR_BOUTON_LANCER = "#47F3A0"; // couleur en hexadecimal du bouton « Lancer ».
+    private final static String COULEUR_BOUTON_ARRETER = "#FD0E0E"; // couleur en hexadecimal du bouton « Arrêter ».
+    final static String COULEUR_CHAMP_DE_TEXTE ="#E6E6E6"; // couleur en hexadecimal des champs de texte qui permettent
+    // à l’utilisateur de faire des choix (JSpinner).
+    private final static String COULEUR_SEPARATIONS="#303030"; // couleur en hexadecimal des lignes de séparation.
+    private final static String COULEUR_POURC_FORET_BRULEE ="FE9E00"; // couleur en hexadecimal d’affichage du pourcentage de la forêt brulée.
+    private final static String COULEUR_BACKGROUND = "#F2F2F2"; //Couleur fond menu
+    private Font police = new Font("Verdana", Font.PLAIN, 10);
+    private JLabel configLabel = new JLabel("Configurer la foret");//Le label "Configurer la fenetre"
+    public Menu(){
+        largeur = 100;
+        this.setBackground( Color.decode(COULEUR_BACKGROUND) );
+        this.setMaximumSize(new Dimension(250, 3000));
+        this.setBorder(BorderFactory.createLineBorder(Color.black));
 
-
+        //Partie Config
+        configLabel.setFont(police);
+        configLabel.setPreferredSize(new Dimension(150, 30));
+        configLabel.setHorizontalAlignment(JLabel.LEFT);
+        configLabel.setBackground(Color.BLUE);
+        configLabel.setForeground(Color.black);
+        configLabel.setBorder(new MatteBorder(0, 0, 1, 0, Color.decode(COULEUR_SEPARATIONS)));
+        this.add(configLabel);
+    }
 
 
 }
