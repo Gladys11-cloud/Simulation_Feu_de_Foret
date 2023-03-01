c2a61763a2c2842097740fb6fe9e5ba3fa23b681
Amine Haddou <haddouamine2002@gmail.com>
2023-03-01 09:08:36 +0300

    - ajout des getter - ajout de toute les composantes de l'interface - ajout de getter et setter dans App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,6 @@ public class App extends JFrame {
     private int min_width ; //largeur minimale de la fenêtre
     private int min_height ; // hauteur minimale de la fenêtre
     private String title ; //titre de la fenêtre
-    private JPanel appPanel;
     private Grille grille ; //c’est la grille qui modélise la forêt
     private Menu menu ; //c’est le panneau contenant le menu de l’application
     private int densite; //densite des arbre dans la forêt en pourcentage
@@ -14,18 +13,26 @@ public class App extends JFrame {
     private int taux_arbres_peu_inflammable ; //taux en pourcentage d’arbre à faible inflammabilité
     private int taux_cellules_humides ; //taux en pourcentage de cellules ayant un sol humide
     private int taux_foret_deja_brulee ; //taux en pourcentage de forêt déjà brulée
+    boolean stopSimulation;
 
     /**
      * Cette classe est une extension (« extend ») de la classe JFrame de Java
      * Elle permet de creer la fenetre et puis d'y ajouter les Panel Menu et Grille
      * Puis de lancer la simulation
      */
-    public App (int minWidth, int minHeight){
-        min_width = minWidth;
-        min_height = minHeight;
-        menu = new Menu();
+    public App (){
+        min_width = 800;
+        min_height = 750;
+        menu = new Menu(this);
         grille = new Grille();
-        appPanel = new JPanel();
+        stopSimulation = false;
+        //On recupere les valeurs des variables dans les spinner de Menu
+        densite = menu.getDensite();
+        taux_cellules_humides = menu.getHumidite();
+        taux_arbres_peu_inflammable = menu.getTauxInflammable();
+        Saison = menu.getSaison();
+        direction_vent = menu.getDirectionVent();
+
         fenetreApp();
         panelsApp();
     }
@@ -49,12 +56,26 @@ public class App extends JFrame {
      * Methode pour creer les Panels et les ajouters dans un seul panel. On ajoutera ce panel a la fenetre.
      */
     private void panelsApp(){
-
         this.add(grille);
         this.add(menu);
         this.setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
     }
+    protected void genererGrille (int nbLignes, int nbColonnes){
+
+    }
+    protected void lancerSimulation(){
+
+    }
+    public void setStopSimulation(boolean var){
+        stopSimulation = var;
+    }
+    public boolean getStopSimulation(){
+        return stopSimulation;
+    }
+    public int getTaux_foret_deja_brulee() {
+        return taux_foret_deja_brulee;
+    }
     public static void main(String[] args){
-        new App(500, 500);
+        new App();
     }
 }
