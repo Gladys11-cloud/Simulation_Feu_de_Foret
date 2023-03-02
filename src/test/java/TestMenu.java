27ad3b472dc985b4edac2a9f0aaf11b80298f876
Amine Haddou <haddouamine2002@gmail.com>
2023-03-02 16:15:48 +0300

    - AJOUT DANS BUILD.GRADLE DE LA LIGNE PERMETTANT DE FAIRE LES TESTS - Methode testGetDensiteDefault()

--- src/test/java/TestMenu.java
+++ src/test/java/TestMenu.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+
 public class TestMenu {
+
+    @Test
+    public void testGetDensiteDefault() {
+        Menu menu = new Menu(new App());
+        assertThat(menu.getDensite()).isEqualTo(50);
+    }
+
 }
